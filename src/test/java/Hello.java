8d4409c2a7602e14c4f9a8b3f8e102cb9f21123f
AliNijat <alinijat968@gmail.com>
2021-06-30 19:29:39 -0400

    New files committed

--- src/test/java/Hello.java
+++ src/test/java/Hello.java
@@ -2,6 +2,7 @@ public class Hello {
 
     public static void main(String[] args) {
 
-        System.out.println("Hello world for test and recap");
+        System.out.println("Hello world for test and recap!!!");
+        System.out.println("Today's is for recap!!!");
     }
 }
