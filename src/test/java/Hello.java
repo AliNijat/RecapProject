7e7d37f628ca11bfdc8beeb2058f0f58955ad16e
AliNijat <alinijat968@gmail.com>
2021-06-30 19:32:36 -0400

    Third changes committed

--- src/test/java/Hello.java
+++ src/test/java/Hello.java
@@ -2,7 +2,7 @@ public class Hello {
 
     public static void main(String[] args) {
 
-        System.out.println("Hello world for test and recap!!!");
+        System.out.println("Hello world!");
         System.out.println("Today's is for recap!!!");
     }
 }
