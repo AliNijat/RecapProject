9682260f286b94e73130b55a6b3791d7e6ca82a7
AliNijat <alinijat968@gmail.com>
2021-06-30 20:25:12 -0400

    work 3

--- src/test/java/UserStory101.java
+++ src/test/java/UserStory101.java
@@ -2,5 +2,7 @@ public class UserStory101 {
     // User story 101
     public static void main(String[] args) {
         // work 2
+
+        System.out.println("User story 101 committed");
     }
 }
